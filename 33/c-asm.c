2da13fdb8eb0317f14cb039bc21cd8f2875f3120
Roshan <115006790+roshan-nahsor@users.noreply.github.com>
2025-08-19 14:12:47 +0000

    for, do, while, continue

---
+++ 33/c-asm.c
@@ -0,0 +1,28 @@
+void piyush() {
+    int i=10;
+    int j=2;
+
+    // do {
+    //     j+=1;
+    //     i-=1;
+    // } while(i>0);
+
+
+    // while(i>0) {
+    //     j+=1;
+    //     i-=1;
+    // }
+
+
+    // for(;i>0; i--) {
+    //     j+=1;
+    // }
+
+
+    for(;i>0; i--) {
+        continue;
+        j+=1;
+    }
+
+    return;
+}
