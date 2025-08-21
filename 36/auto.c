68eabf8ab681f6a1bd49cfce9e450947ecb9fd3b
Roshan <115006790+roshan-nahsor@users.noreply.github.com>
2025-08-21 07:19:49 +0000

    my experiment of auto

--- 36/auto.c
+++ 36/auto.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+void foo(int a) {
+    printf("%d\n",a);
+} 
+
 void main() {
     {
         auto int a=10;
+        printf("%d\n", a);
+        foo(a);
     }
-    printf("%d\n", a);
+    // printf("%d\n", a);
 
     return;
 }
