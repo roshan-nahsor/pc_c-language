2b98db12929173c43777566245730b116678c9f8
Roshan <115006790+roshan-nahsor@users.noreply.github.com>
2025-08-27 10:30:07 +0000

    arithmetic operations

---
+++ 43/arith.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    int v1, v2, ans;
+
+    v1=2, v2=3;
+
+    printf("+ %d\n", v1+v2);
+    printf("- %d\n", v1-v2);
+    printf("* %d\n", v1*v2);
+    printf("/ %d\n", v1/v2);
+    printf("%% %d\n", v1%v2);
+    printf("ans %d\n", ans);
+}
