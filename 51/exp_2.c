efdf59cedc99a2b286b03c2f57b8726bae6d962f
roshan-nahsor <roshanb1943@gmail.com>
2026-03-11 21:14:56 +0530

    experiment 2

---
+++ 51/exp_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a[5]={1,2,3,4,5};
+    printf("sizeof(a)=%ld\n",sizeof(a));
+
+    for(int i=0; i<sizeof(a)/sizeof(int); i++) {
+        printf("a[%d]=%d\n",i,a[i]);
+    }
+}
